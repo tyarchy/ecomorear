5e1d7a42b564b6ea77bc062a119a834144806cba
tyarchy <ty.d.gregory@gmail.com>
2022-03-20 13:13:05 -0600

    model setup and api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,25 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          ''
+      
+      ]
+      }
+    ]
+
 });
 
 router.get('/:id', (req, res) => {
